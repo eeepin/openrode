478f4d7dd7f6bf52829ae91d88360ed7076c6950
Eepin <eepin.wu@gmail.com>
2026-06-02 00:03:53 +0800

    feat: the minimal bin app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use hillm::client;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(short = 'p', long)]
+    prompt: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    println!("{}", args.prompt);
+    Ok(())
 }
